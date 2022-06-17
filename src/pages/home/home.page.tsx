51620fc75296aa0baf573f1e1bfd6b565d80b0de
Ali <alifarajzade1@yahoo.com>
2022-06-17 15:42:14 +0430

    Add exercises component.

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,12 +1,13 @@
 import { Box } from '@mui/material'
 import { useState } from 'react'
+import Exercises from '../../components/exercises/exercises.component'
 import HeroBanner from '../../components/hero-banner/hero-banner.component'
 import SearchExercise from '../../components/search-exercise/search-exercies.component'
 import { TExercise } from '../../types/exercise.types'
 
 const Home: React.FC = () => {
     const [bodyPart, setBodyPart] = useState<string>('all')
-    const [exercies, setExercises] = useState<TExercise[]>([])
+    const [exercises, setExercises] = useState<TExercise[]>([])
 
     return (
         <Box>
@@ -15,6 +16,11 @@ const Home: React.FC = () => {
                 bodypart={bodyPart}
                 setBodyPart={setBodyPart}
                 setExercises={setExercises}
+            />
+            <Exercises
+                bodypart={bodyPart}
+                setExercises={setExercises}
+                exercises={exercises}
             />
         </Box>
     )
