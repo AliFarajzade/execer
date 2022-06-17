d8b91c66a71c1105d7b2b554007079c8e033fd8b
Ali <alifarajzade1@yahoo.com>
2022-06-17 16:34:55 +0430

    Create exercise page and datails component.

--- src/pages/exercise-datails/exercise-datails.page.tsx
+++ src/pages/exercise-datails/exercise-datails.page.tsx
@@ -1,5 +1,46 @@
+import { Box } from '@mui/material'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import Details from '../../components/details/details.component'
+import { requestOptions } from '../../helpers/request-options.helper'
+import { TExercise } from '../../types/exercise.types'
+
 const ExerciseDetails: React.FC = () => {
-    return <div>ExerciseDetails</div>
+    const { id } = useParams()
+
+    const [exerciseDetails, setExerciseDetails] = useState<TExercise | null>(
+        null
+    )
+    const [isExerciseDetailsLoading, setIsExerciseDetailsLoading] =
+        useState<boolean>(false)
+
+    useEffect(() => {
+        setIsExerciseDetailsLoading(true)
+        const fetchExercisesData = async () => {
+            const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com'
+            const youtubeSearchUrl =
+                'https://youtube-search-and-download.p.rapidapi.com'
+
+            const response = await fetch(
+                `${exerciseDbUrl}/exercises/exercise/${id}`,
+                requestOptions
+            )
+
+            const exerciseDetailData = await response.json()
+            setExerciseDetails(exerciseDetailData)
+        }
+        setIsExerciseDetailsLoading(false)
+
+        fetchExercisesData()
+    }, [id])
+
+    console.log(exerciseDetails)
+
+    return (
+        <Box>
+            {exerciseDetails && <Details exerciseDetails={exerciseDetails} />}
+        </Box>
+    )
 }
 
 export default ExerciseDetails
