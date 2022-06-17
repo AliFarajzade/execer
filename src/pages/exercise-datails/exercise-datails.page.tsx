b57784610c4bf676f7f355d58ace1d47accf9c27
Ali <alifarajzade1@yahoo.com>
2022-06-17 17:19:23 +0430

    Fetch related videos and add related videos components.

--- src/pages/exercise-datails/exercise-datails.page.tsx
+++ src/pages/exercise-datails/exercise-datails.page.tsx
@@ -2,7 +2,11 @@ import { Box } from '@mui/material'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Details from '../../components/details/details.component'
-import { exerciseRequestOptions } from '../../helpers/request-options.helper'
+import RelatedVideos from '../../components/related-videos/related-videos.component'
+import {
+    exerciseRequestOptions,
+    youtubeRequestOptions,
+} from '../../helpers/request-options.helper'
 import { TExercise } from '../../types/exercise.types'
 
 const ExerciseDetails: React.FC = () => {
@@ -14,22 +18,37 @@ const ExerciseDetails: React.FC = () => {
     const [isExerciseDetailsLoading, setIsExerciseDetailsLoading] =
         useState<boolean>(false)
 
+    const [youtubeData, setYoutubeData] = useState<any | null>(null)
+    const [isYoutubeLoading, setIsYoutubeLoading] = useState<boolean>(false)
+
     useEffect(() => {
-        setIsExerciseDetailsLoading(true)
         const fetchExercisesData = async () => {
             const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com'
             const youtubeSearchUrl =
                 'https://youtube-search-and-download.p.rapidapi.com'
 
-            const response = await fetch(
+            //
+            setIsExerciseDetailsLoading(true)
+            const exerciseResponse = await fetch(
                 `${exerciseDbUrl}/exercises/exercise/${id}`,
                 exerciseRequestOptions
             )
 
-            const exerciseDetailData = await response.json()
+            const exerciseDetailData = await exerciseResponse.json()
             setExerciseDetails(exerciseDetailData)
+            setIsExerciseDetailsLoading(false)
+
+            //
+            setIsYoutubeLoading(true)
+            const youtubeSearchResponse = await fetch(
+                `${youtubeSearchUrl}/search?query=${exerciseDetailData.name} exercise`,
+                youtubeRequestOptions
+            )
+            const youtubeSearchData = await youtubeSearchResponse.json()
+            setYoutubeData(youtubeSearchData)
+            setIsYoutubeLoading(false)
+            //
         }
-        setIsExerciseDetailsLoading(false)
 
         fetchExercisesData()
     }, [id])
@@ -38,7 +57,17 @@ const ExerciseDetails: React.FC = () => {
 
     return (
         <Box>
-            {exerciseDetails && <Details exerciseDetails={exerciseDetails} />}
+            {exerciseDetails && (
+                <>
+                    <Details exerciseDetails={exerciseDetails} />
+                    {youtubeData && (
+                        <RelatedVideos
+                            youtubeData={youtubeData}
+                            exerciseName={ExerciseDetails.name}
+                        />
+                    )}
+                </>
+            )}
         </Box>
     )
 }
