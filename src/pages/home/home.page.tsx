a7b1467f212b97f0a617093a22f71b286508e068
Ali <alifarajzade1@yahoo.com>
2022-06-17 13:15:45 +0430

    Add search exercise component to home page.

--- src/pages/home/home.page.tsx
+++ src/pages/home/home.page.tsx
@@ -1,10 +1,21 @@
 import { Box } from '@mui/material'
+import { useState } from 'react'
 import HeroBanner from '../../components/hero-banner/hero-banner.component'
+import SearchExercise from '../../components/search-exercise/search-exercies.component'
+import { TExercise } from '../../types/exercise.types'
 
 const Home: React.FC = () => {
+    const [bodyPart, setBodyPart] = useState<string>('all')
+    const [exercies, setExercises] = useState<TExercise[]>([])
+
     return (
         <Box>
             <HeroBanner />
+            <SearchExercise
+                bodypart={bodyPart}
+                setBodyPart={setBodyPart}
+                setExercises={setExercises}
+            />
         </Box>
     )
 }
